017597309c6690b1dcc25a8c6a0cf88fa877cbee
Rhesmika <Rhesmika@gmail.com>
2022-06-04 15:29:07 +0000

    Add document event listeners and assign button event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-function runGame(){}
+console.log("hello");
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "play-again"){
+            runGame();
+        } else {
+            let handType = this.getAttribute("datatype");
+            alert (`You picked ${handType}`);
+            console.log("hand type")
+        }
+        })
+    }
+})
+function runGame(){
+    console.log("run Game");
+}
 
 function checkAnswer(){}
 
